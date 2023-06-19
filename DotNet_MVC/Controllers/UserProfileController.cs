8b391f6ec403a8bac97af3ce4356c233cc358c47
Ashok Basnet <87795084+ashokbasnetofficial@users.noreply.github.com>
2023-06-19 23:28:19 +0545

    display data from data base

--- DotNet_MVC/Controllers/UserProfileController.cs
+++ DotNet_MVC/Controllers/UserProfileController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dotnet_Mvc.Data;
+using Dotnet_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
 namespace Dotnet_Mvc.Controllers{
     public class UserProfileController:Controller
     {
+
+        private readonly ApplicationDbContext _db;
+        public UserProfileController(ApplicationDbContext db)
+        {
+                _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+          //  var userProfile = _db.UserProfile.ToList();
+          List<UserProfile> users = _db.UserProfile.ToList();
+            return View(users);
         }
     }
 }
